d4873bf5b165c2d4c7bed95369f9238f3d08ce3c
Adam Harpur <adam@harpur.io>
2019-04-26 21:29:56 -0500

    feat: stubbed out modules

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 var Nightmare = require('nightmare'),
     nightmare = Nightmare();
+var ads = require('./config/ads');
+var post = require('./modules/post');
+var login = require('./modules/login');
+var checkLogin = require('./modules/checkLogin');
+
+const url = ""
+
+async function run() {
+    var loggedIn = await checkLogin();
+    switch (loggedIn) {
+        case true:
+            console.log('Login successful')
+            main(ads)
+            break;
+        default:
+            console.log('Login check failed, attemping login');
+            loggedIn = await login()
+    }
+}
+
+
+function main() {
+    console.log(`Running main function`)
+}
+
+
+run()
