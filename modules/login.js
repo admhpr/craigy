237a9d537b0a262b41ddddf17d5d6ac485822415
Adam Harpur <adam@harpur.io>
2019-04-26 21:38:47 -0500

    feat: started login

--- modules/login.js
+++ modules/login.js
@@ -1,4 +1,21 @@
+var Nightmare = require('nightmare'),
+    nightmare = Nightmare({
+        show: true
+    });
+
 module.exports =
-    function () {
-        console.log('login')
+    async function () {
+        var loggedIn;
+        console.log(`Attemping login...`)
+        nightmare.goto(`https://accounts.craigslist.org/login`)
+            .wait(2000)
+            .insert('#inputEmailHandle', 'email@email.com')
+            .insert('#inputPassword', 'password')
+            .click('.login-box .accountform-btn')
+            .wait(200)
+            .title()
+            .end()
+        nightmare.then(function (result) {
+            console.log(result)
+        })
     }
