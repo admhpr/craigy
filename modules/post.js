a4589c5570b4d36291b8b2b689a4a74a817c60cb
Adam Harpur <adam@harpur.io>
2019-04-27 18:42:28 -0500

    feat: additions to post module

--- modules/post.js
+++ modules/post.js
@@ -1,13 +1,21 @@
 var Nightmare = require('nightmare'),
 nightmare = Nightmare();
+
+var log = require('../utils/logger');
+
 module.exports =
     function (dto) {
-        var {city} = dto;
+        // var {city} = dto;
+        city = "jkdsjdskj"
         var url = `https://${city}.craigslist.org`
         console.log('post', url)
         //TODO:
-        nightmare.goto(url)
-        .wait(2000)
-        .click('#postlks #post')
-        .click('input[value=fso]')
+        try{
+            nightmare.goto(url)
+            .wait(2000)
+            .click('#postlks #post')
+            .click('input[value=fso]')
+        }catch(e){
+            log.error(e.message)
+        }
     }
