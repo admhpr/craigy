a70b3f53f3d8a9bcf122e9d6f423e6d864b3e046
Adam Harpur <adam@harpur.io>
2019-04-27 18:38:41 -0500

    feat: start post module

--- modules/post.js
+++ modules/post.js
@@ -1,4 +1,13 @@
+var Nightmare = require('nightmare'),
+nightmare = Nightmare();
 module.exports =
     function (dto) {
-        console.log('post', dto)
+        var {city} = dto;
+        var url = `https://${city}.craigslist.org`
+        console.log('post', url)
+        //TODO:
+        nightmare.goto(url)
+        .wait(2000)
+        .click('#postlks #post')
+        .click('input[value=fso]')
     }
