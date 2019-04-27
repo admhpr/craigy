f7bd0b8387ea390060780f13739e9a7903a23655
Adam Harpur <adam@harpur.io>
2019-04-26 21:56:26 -0500

    feat: login function

--- modules/login.js
+++ modules/login.js
@@ -1,26 +1,27 @@
-var Nightmare = require('nightmare'),
-    nightmare = Nightmare({
-        show: true
-    });
-
 const {
     USER_EMAIL,
-    USER_PASSWORD
+    USER_PASSWORD,
+    SHOW_BROWSER,
 } = process.env
+var Nightmare = require('nightmare'),
+    nightmare = Nightmare({
+        show: SHOW_BROWSER
+    });
 
 module.exports =
     async function () {
         var loggedIn;
         console.log(`Attemping login...`)
         nightmare.goto(`https://accounts.craigslist.org/login`)
-            .wait(2000)
+            .wait(1000)
             .insert('#inputEmailHandle', USER_EMAIL)
             .insert('#inputPassword', USER_PASSWORD)
             .click('.login-box .accountform-btn')
-            .wait(200)
+            .wait(1000)
             .title()
             .end()
-        nightmare.then(function (result) {
-            console.log(result)
+        loggedIn = await nightmare.then(function (result) {
+            return result === 'craigslist account'
         })
+        return loggedIn
     }
