690119269b47892f31ce705760568890163b7c34
Adam Harpur <adam@harpur.io>
2019-04-28 09:13:17 -0500

    feat: image upload and value entry

--- modules/post.js
+++ modules/post.js
@@ -1,28 +1,32 @@
 
 
 
-const { USER_ZIP, USER_STREET, USER_CITY, USER_TEL} = process.env;
+const { USER_ZIP, USER_STREET, USER_CITY, USER_TEL, USER_EMAIL} = process.env;
 var log = require('../utils/logger');
 
 module.exports = async function (dto) {
-        var Nightmare = require('nightmare'),
-        nightmare = Nightmare({show: true});
+        var Nightmare = require('nightmare');
         require('nightmare-upload')(Nightmare);
+        var nightmare = Nightmare({show: true});
         
         var {city, title, price, body, image} = dto;
+        console.log(image)
         var url = `https://${city}.craigslist.org`
         
         try{
             await nightmare.goto(url)
-            .wait(5000)
+            .wait(2000)
+            // create post
             .click('#postlks #post')
             .click('input[value=fso]')
             .click('.pickbutton')
-            .wait(1000)
-            //TODO:
-            .click('input[value=5]')
+            .wait(2000)
+            // post type
+            // general for sale
+            .click('#new-edit > div > label > label:nth-child(28) > input')
             .click('.pickbutton')
-            .wait(1000)
+            .wait(1200)
+            // post values
             .insert('#PostingTitle', title)
             .insert('.price > .json-form-input', price)
             .insert('#GeographicArea', USER_CITY)
@@ -30,17 +34,23 @@ module.exports = async function (dto) {
             .insert('#PostingBody', body)
             .click('.see_my_other')
             .click('.contact_text_ok')
-            .insert('contact_phone > .json-input-form', USER_TEL)
+            .insert('label.json-form-item.text.contact_phone.variant-tel > label > input', USER_TEL)
             .insert('.xstreet0 > .json-form-input', USER_STREET)
             .insert('.city > .json-form-input', USER_CITY)
-            .click('input[value=continue]')
+            .insert('.email-form-text-input > label > label > input', USER_EMAIL)
+            .wait(500)
+            .click('.submit-buttons > div > button')
             .wait(1000)
-            .click('.continue .bigbutton')
-            .wait(1000)
-            .upload('#plupload', image)
-            .wait(1000)
-            .click('.done .bigbutton')
-            .click('input[value=Continue]')
+            // map
+            .click('#leafletForm > button')
+            .wait(2000)
+            // upload image
+            .upload('.moxie-shim > input:nth-child(1)', image)
+            .wait(3000)
+            // cotinue
+            .click('body > article > section > form > button')
+            // publish
+            .click('#publish_top > button')
         
             await nightmare.end()
         }catch(e){
